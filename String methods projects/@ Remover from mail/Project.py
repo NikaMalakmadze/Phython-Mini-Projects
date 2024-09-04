3f1d28bbba8bcb00143602bdfd5910bee4b12167
NikaMalakmadze <malakmadzenika0909@gmail.com>
2024-09-04 15:34:58 +0400

    String methods projects

---
+++ String methods projects/@ Remover from mail/Project.py
@@ -0,0 +1,7 @@
+# @ remover from mail
+
+mail = input(f"Please enter your mail: ")   
+
+edited_mail = mail.replace("@", "")         #replace @ with nothing
+
+print(f"Your mail without @ is: ", edited_mail)
